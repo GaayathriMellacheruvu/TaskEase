e5c9c391f8bffde5c3b6e4f499cc282fb53b1fc4
Gaayathri Mellacheruvu <95865312+GaayathriMellacheruvu@users.noreply.github.com>
2024-02-05 02:08:29 +0530

    main.py

--- main.py
+++ main.py
@@ -4,23 +4,9 @@ from pymongo import MongoClient
 from datetime import datetime
 from bson import ObjectId
 from pydantic import BaseModel
-from typing import List, Dict, Union
-import os
+from typing import List
 
 app = FastAPI()
-
-# Set your OpenAI API key as an environment variable
-
-@app.on_event("startup")
-async def startup_event():
-    # Add your startup initialization code here
-    pass
-
-def get_collection_name():
-    current_month = datetime.now().strftime("%B").lower()
-    return current_month
-
-router = APIRouter()
 
 # Add CORS middleware
 origins = ["https://task-ease-7f6t.onrender.com"]  # Change this to your frontend's actual domain in production
@@ -42,106 +28,106 @@ class TaskResponse(BaseModel):
 class TaskCreate(BaseModel):
     task_text: str
 
+def get_collection_name():
+    current_month = datetime.now().strftime("%B").lower()
+    return current_month
+
+router = APIRouter()
+
 @router.post("/add_task/")
 async def add_task(task_data: TaskCreate, user_name: str):
     collection_name = get_collection_name()
-    client = MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}")
-    db = client[user_name]
-    collection = db[collection_name]
+    with MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}") as client:
+        db = client[user_name]
+        collection = db[collection_name]
 
-    task_document = {"task_text": task_data.task_text}
-    result = collection.insert_one(task_document)
+        task_document = {"task_text": task_data.task_text}
+        result = collection.insert_one(task_document)
 
     return {"message": "Task added successfully", "task_id": str(result.inserted_id)}
 
 @router.get("/list_tasks/")
 async def list_tasks(user_name: str):
     collection_name = get_collection_name()
-    client = MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}")
-    db = client[user_name]
-    collection = db[collection_name]
+    with MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}") as client:
+        db = client[user_name]
+        collection = db[collection_name]
 
-    tasks = list(collection.find({}, {"_id": 1, "task_text": 1}))
+        tasks = list(collection.find({}, {"_id": 1, "task_text": 1}))
 
-    # Convert ObjectId to string
-    for task in tasks:
-        task["_id"] = str(task["_id"])
+        for task in tasks:
+            task["_id"] = str(task["_id"])
 
     return {"tasks": tasks}
 
 @router.delete("/delete_task/{task_id}/")
 async def delete_task(task_id: str, user_name: str):
     collection_name = get_collection_name()
-    client = MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}")
-    db = client[user_name]
-    collection = db[collection_name]
+    with MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}") as client:
+        db = client[user_name]
+        collection = db[collection_name]
 
-    try:
-        # Convert the input string to ObjectId
-        obj_id = ObjectId(task_id)
-        result = collection.delete_one({"_id": obj_id})
+        try:
+            obj_id = ObjectId(task_id)
+            result = collection.delete_one({"_id": obj_id})
 
-        if result.deleted_count > 0:
-            return {"message": f"Successfully deleted data with ObjectId: {task_id}"}
-        else:
-            return {"message": f"No data found with ObjectId: {task_id}"}
-    except Exception as e:
-        return {"message": "Invalid ObjectId format. Please enter a valid ObjectId."}
+            if result.deleted_count > 0:
+                return {"message": f"Successfully deleted data with ObjectId: {task_id}"}
+            else:
+                return {"message": f"No data found with ObjectId: {task_id}"}
+        except Exception as e:
+            return {"message": "Invalid ObjectId format. Please enter a valid ObjectId."}
 
 @router.put("/update_task/{task_id}/")
 async def update_task(task_id: str, task_data: TaskUpdate, user_name: str):
     collection_name = get_collection_name()
-    client = MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}")
-    db = client[user_name]
-    collection = db[collection_name]
+    with MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}") as client:
+        db = client[user_name]
+        collection = db[collection_name]
 
-    try:
-        # Convert the input string to ObjectId
-        obj_id = ObjectId(task_id)
-        result = collection.update_one({"_id": obj_id}, {"$set": {"task_text": task_data.task_text}})
+        try:
+            obj_id = ObjectId(task_id)
+            result = collection.update_one({"_id": obj_id}, {"$set": {"task_text": task_data.task_text}})
 
-        if result.modified_count > 0:
-            return {"message": f"Successfully updated task with ObjectId: {task_id}"}
-        else:
-            return {"message": f"No data found with ObjectId: {task_id}"}
-    except Exception as e:
-        return {"message": "Invalid ObjectId format. Please enter a valid ObjectId."}
+            if result.modified_count > 0:
+                return {"message": f"Successfully updated task with ObjectId: {task_id}"}
+            else:
+                return {"message": f"No data found with ObjectId: {task_id}"}
+        except Exception as e:
+            return {"message": "Invalid ObjectId format. Please enter a valid ObjectId."}
 
 @router.get("/get_task/{task_id}/")
 async def get_task(task_id: str, user_name: str):
     collection_name = get_collection_name()
-    client = MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}")
-    db = client[user_name]
-    collection = db[collection_name]
+    with MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}") as client:
+        db = client[user_name]
+        collection = db[collection_name]
 
-    try:
-        # Convert the input string to ObjectId
-        obj_id = ObjectId(task_id)
-        task = collection.find_one({"_id": obj_id}, {"_id": 1, "task_text": 1})
+        try:
+            obj_id = ObjectId(task_id)
+            task = collection.find_one({"_id": obj_id}, {"_id": 1, "task_text": 1})
 
-        if task:
-            task["_id"] = str(task["_id"])
-            return {"task": task}
-        else:
-            return {"message": f"No data found with ObjectId: {task_id}"}
-    except Exception as e:
-        return {"message": "Invalid ObjectId format. Please enter a valid ObjectId."}
+            if task:
+                task["_id"] = str(task["_id"])
+                return {"task": task}
+            else:
+                return {"message": f"No data found with ObjectId: {task_id}"}
+        except Exception as e:
+            return {"message": "Invalid ObjectId format. Please enter a valid ObjectId."}
 
 @router.delete("/delete_all_tasks/")
 async def delete_all_tasks(user_name: str):
     collection_name = get_collection_name()
-    client = MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}")
-    db = client[user_name]
-    collection = db[collection_name]
+    with MongoClient(f"mongodb+srv://taskease:102938@cluster0.kavkfm1.mongodb.net/{user_name}") as client:
+        db = client[user_name]
+        collection = db[collection_name]
 
-    result = collection.delete_many({})
+        result = collection.delete_many({})
 
-    if result.deleted_count > 0:
-        return {"message": "All tasks deleted successfully"}
-    else:
-        return {"message": "No tasks found to delete"}
-
-
+        if result.deleted_count > 0:
+            return {"message": "All tasks deleted successfully"}
+        else:
+            return {"message": "No tasks found to delete"}
 
 # Include the router in the main app
 app.include_router(router, prefix="/tasks")
